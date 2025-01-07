d77c232cd335e2ce633f1953c87aaabeeebcab9b
Roland Lariotte <reel.errors.0z@icloud.com>
2025-01-07 10:08:10 +0100

    [2.3] Creating and add the CanvasBlog project to the workspace

---
+++ CanvasBlog/CanvasBlog/CanvasBlogApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2025 Roland Lariotte. Under the MIT License.
+//
+
+import SwiftUI
+
+@main
+struct CanvasBlogApp: App {
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
