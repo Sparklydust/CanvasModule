5e6ea87ee3834f555d237202f0310fe570a9aa0d
Roland Lariotte <reel.errors.0z@icloud.com>
2025-01-07 10:28:07 +0100

    [2.4] Creating and adding CanvasPlay preview project to the workspace

---
+++ CanvasPlay/CanvasPlay/CanvasPlayApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2025 Roland Lariotte. Under the MIT License.
+//
+
+import SwiftUI
+
+@main
+struct CanvasPlayApp: App {
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
