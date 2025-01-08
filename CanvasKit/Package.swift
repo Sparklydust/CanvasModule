a00341a89df39adb10cb12c6dfc2cc61cb0a5492
Roland Lariotte <reel.errors.0z@icloud.com>
2025-01-08 09:25:08 +0100

    [2.7] Configuring the CanvasKit Swift Package Manager

--- CanvasKit/Package.swift
+++ CanvasKit/Package.swift
@@ -4,11 +4,28 @@ import PackageDescription
 
 let package = Package(
   name: "CanvasKit",
+  platforms: [
+    .iOS(.v18)
+  ],
   products: [
     .library(name: "CanvasKit", targets: ["CanvasKit"])
   ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.6")
+  ],
   targets: [
     .target(name: "CanvasKit"),
-    .testTarget(name: "CanvasKitTests", dependencies: ["CanvasKit"])
+    .testTarget(
+      name: "CanvasKitTests",
+      dependencies: ["CanvasKit"]
+    ),
+    .testTarget(
+      name: "CanvasKitUITests",
+      dependencies: [
+        "CanvasKit",
+        .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+      ],
+      exclude: ["Snapshots"]
+    )
   ]
 )
---
+++ CanvasKit/Tests/CanvasKitUITests/CanvasKitUITests.swift
@@ -0,0 +1,10 @@
+//
+// Copyright © 2025 Roland Lariotte. Under the MIT License.
+//
+
+import Testing
+@testable import CanvasKit
+
+@Test func example() async throws {
+  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
