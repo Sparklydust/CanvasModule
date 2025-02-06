ab0f8c0d61e8130b9001214a1d0a6b03efaf942b
Roland Lariotte <reel.errors.0z@icloud.com>
2025-02-06 17:35:42 +0100

    [10.4] Modeling the input field

---
+++ CanvasKit/Sources/CanvasKit/Objects/Enums/CKTextFieldMode.swift
@@ -0,0 +1,16 @@
+//
+// Copyright © 2025 Roland Lariotte. Under the MIT License.
+//
+
+import Foundation
+
+/// Represents the mode of a text field.
+enum CKTextFieldMode: String {
+
+  /// The default state of the text field.
+  case `default`
+  /// The state when the text field is focused.
+  case focused
+  /// The state when the text field is filled with content.
+  case filled
+}
---
+++ CanvasKit/Sources/CanvasKit/Objects/Models/TextFieldFocus.swift
@@ -0,0 +1,23 @@
+//
+// Copyright © 2025 Roland Lariotte. Under the MIT License.
+//
+
+import Foundation
+
+/// Represents the focus state of a text field.
+struct TextFieldFocus: Equatable {
+
+  /// The unique identifier of the focused text field.
+  let id: Int
+
+  /// Initializes a new focus state with an identifier.
+  /// - Parameter id: The unique identifier for the text field focus.
+  init(id: Int) {
+    self.id = id
+  }
+
+  /// Returns the next focus state by incrementing the identifier.
+  func next() -> TextFieldFocus {
+    TextFieldFocus(id: id + 1)
+  }
+}
